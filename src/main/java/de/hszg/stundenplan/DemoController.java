9e6fb8cc71aebb757cb7f4cdfeb8148d068e9596
nanewtech <itznanewtech@gmail.com>
2026-05-11 14:07:20 +0200

    dateistruktur erstellt

---
+++ src/main/java/de/hszg/stundenplan/DemoController.java
@@ -0,0 +1,18 @@
+package de.hszg.stundenplan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/test/")
+    public String test() {
+        return "test!";
+    }
+}
