4e56c23a77c631080db280f01071fdf9054a2742
nanewtech <itznanewtech@gmail.com>
2026-05-12 16:20:29 +0200

    Add: Environment variable support

--- backend/src/main/java/de/hszg/stundenplaner/Demo.java
+++ backend/src/main/java/de/hszg/stundenplaner/Demo.java
@@ -2,10 +2,20 @@ package de.hszg.stundenplaner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Demo {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                        .directory("./") 
+                        .filename(".env")
+                        .load();
+
+        dotenv.entries().forEach(entry -> 
+                    System.setProperty(entry.getKey(), entry.getValue())
+                );
+
         SpringApplication.run(Demo.class, args);
     }
 }
