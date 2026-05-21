29517a157c672c5404c3a8d61512291b005a27f1
Laefyhe <shiningvoiduwu@gmail.com>
2026-05-21 22:02:11 +0200

    schwierigkeitsgrad auf int statt varchar geändert und die fehlenden ifs in den create table anweisungen hinzugefügt

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -5,37 +5,37 @@ CREATE TABLE IF NOT EXISTS hibernate_sequence (
 INSERT INTO hibernate_sequence (next_val) SELECT 1 WHERE NOT EXISTS (SELECT 1 FROM hibernate_sequence);
 
 
-CREATE TABLE NOT EXISTS raum (
+CREATE TABLE IF NOT EXISTS raum (
     id BIGINT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     kapazitaet INT NOT NULL,
     ausstattung TEXT
 );
 
-CREATE TABLE NOT EXISTS matrikel (
+CREATE TABLE IF NOT EXISTS matrikel (
     id BIGINT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     anzahl INT NOT NULL
 );
 
-CREATE TABLE NOT EXISTS professor (
+CREATE TABLE IF NOT EXISTS professor (
     id BIGINT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     verfuegbarkeit VARCHAR(255)
 );
 
 
-CREATE TABLE NOT EXISTS modul (
+CREATE TABLE IF NOT EXISTS modul (
     id BIGINT PRIMARY KEY,
     raum_id BIGINT,
     name VARCHAR(150) NOT NULL,
     sws INT NOT NULL, 
-    schwierigkeitsgrad VARCHAR(50),
+    schwierigkeitsgrad INT,
     raumanforderung TEXT,
     FOREIGN KEY (raum_id) REFERENCES raum(id) ON DELETE SET NULL
 );
 
-CREATE TABLE NOT EXISTS student (
+CREATE TABLE IF NOT EXISTS student (
     id BIGINT PRIMARY KEY,
     matrikel_id BIGINT,
     name VARCHAR(100) NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE NOT EXISTS student (
 );
 
 
-CREATE TABLE NOT EXISTS modul_professor (
+CREATE TABLE IF NOT EXISTS modul_professor (
     modul_id BIGINT,
     professor_id BIGINT,
     PRIMARY KEY (modul_id, professor_id),
@@ -52,7 +52,7 @@ CREATE TABLE NOT EXISTS modul_professor (
     FOREIGN KEY (professor_id) REFERENCES professor(id) ON DELETE CASCADE
 );
 
-CREATE TABLE NOT EXISTS vorlesung (
+CREATE TABLE IF NOT EXISTS vorlesung (
     id BIGINT PRIMARY KEY,
     modul_id BIGINT NOT NULL,
     matrikel_id BIGINT NOT NULL,
@@ -62,7 +62,7 @@ CREATE TABLE NOT EXISTS vorlesung (
     FOREIGN KEY (matrikel_id) REFERENCES matrikel(id) ON DELETE CASCADE
 );
 
-CREATE TABLE NOT EXISTS gaststudent (
+CREATE TABLE IF NOT EXISTS gaststudent (
     vorlesung_id BIGINT,
     student_id BIGINT,
     PRIMARY KEY (vorlesung_id, student_id),
